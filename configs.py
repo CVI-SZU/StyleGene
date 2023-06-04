8a5b809a84983dd94048be63052ac10253a146f2
wmpscc <heolis1211@gmail.com>
2023-06-04 17:09:06 +0800

    update

--- configs.py
+++ configs.py
@@ -1,9 +1,9 @@
 path_ckpt_landmark68 = "checkpoints/shape_predictor_68_face_landmarks.dat.bz2"
-path_ckpt_e4e = "/home/cvi_demo/PythonProject/StyleGene/checkpoints/e4e_ffhq_encode.pt"
-path_ckpt_stylegan2 = '/home/cvi_demo/PythonProject/StyleGene/checkpoints/stylegan2-ffhq-config-f.pt'
-path_ckpt_stylegene = "/home/cvi_demo/PythonProject/StyleGene/checkpoints/stylegene_N18.ckpt"
-path_ckpt_fairface = '/home/cvi_demo/PythonProject/StyleGene/checkpoints/res34_fair_align_multi_7_20190809.pt'
-path_ckpt_genepool = "/home/cvi_demo/PythonProject/StyleGene/checkpoints/geneFactorPool.pkl"
+path_ckpt_e4e = "checkpoints/e4e_ffhq_encode.pt"
+path_ckpt_stylegan2 = 'checkpoints/stylegan2-ffhq-config-f.pt'
+path_ckpt_stylegene = "checkpoints/stylegene_N18.ckpt"
+path_ckpt_fairface = 'checkpoints/res34_fair_align_multi_7_20190809.pt'
+path_ckpt_genepool = "checkpoints/geneFactorPool.pkl"
 
 path_csv_ffhq_attritube = 'data/fairface_gender_angle.csv'
 path_dataset_ffhq = None
